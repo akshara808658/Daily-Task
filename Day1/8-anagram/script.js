a5324312f2e2ffbbdf6e6b6f2f1272910aa5d105
Akshara <pakshara.2005@gmail.com>
2025-10-15 23:19:45 +0530

    Day 1

---
+++ Day1/8-anagram/script.js
@@ -0,0 +1,28 @@
+//outPut
+/**
+ [
+  ["care", "race", "acre"],
+  ["dog", "god"],
+  ["cat"]
+]
+ */
+// const arr = ['care','race','acre','dog','god','cat'];
+// function anagramTogether(arr){
+//   let result = [];
+//   let w1 = 'care';
+//   let w2 = 'dog';
+//   let w3 = 'cat'
+//   for(let i = 0; i< arr.length; i++){
+//   if(w1.includes(arr[i])){
+//     result.push(arr[i])
+//   }
+//     if(w2.includes(arr[i])){
+//     result.push(arr[i])
+//   }
+//     if(w3.includes(arr[i])){
+//     result.push(arr[i])
+//   }
+//   }
+//   return result;
+// }
+// console.log(anagramTogether(arr))
